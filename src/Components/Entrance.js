04d6a6a733fffa5238e52a52fe05234a5d0afc57
Oleh Zinchenko <oleh.v.zinchenko@gmail.com>
2023-12-12 17:24:28 +0200

    Fix bugs

--- src/Components/Entrance.js
+++ src/Components/Entrance.js
@@ -3,28 +3,28 @@ import {Input} from "./Input";
 import {useState} from "react";
 
 export const Entrance = () => {
-    const [number1, setNumber1] = useState(0)
-    const [number2, setNumber2] = useState(0)
-    const [number3, setNumber3] = useState(0)
+    const [floors, setFloors] = useState(0)
+    const [flats, setFlats] = useState(0)
+    const [flatNumber, setFlatNumber] = useState(0)
     const [result, setResult] = useState(0)
 
     const Calc = () => {
-        setResult(Math.floor((number3-1)%(number1*number2)/number2+1))
+        setResult(Math.floor((flatNumber-1)%(floors*flats)/flats+1))
     }
 
     return (
         <div>
             <div>
                 <span>Please enter floors number: </span>
-                <Input value={number1} setValue={setNumber1}/>
+                <Input value={floors} setValue={setFloors}/>
             </div>
             <div>
                 <span>Please enter number of flats per floor: </span>
-                <Input value={number2} setValue={setNumber2}/>
+                <Input value={flats} setValue={setFlats}/>
             </div>
             <div>
                 <span>Please enter number of flat: </span>
-                <Input value={number3} setValue={setNumber3}/>
+                <Input value={flatNumber} setValue={setFlatNumber}/>
             </div>
             <button onClick={Calc}>Calculate</button>
             <div>
