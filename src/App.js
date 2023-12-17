0e4ead96779e1115a7148b94386b4ae5f6206d0c
Oleh Zinchenko <oleh.v.zinchenko@gmail.com>
2023-12-17 18:33:40 +0200

    My Mac commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import {Greetings} from "./Components/Greetings";
 import {Gopni4ek} from "./Components/Gopni4ek"
@@ -24,30 +23,3 @@ function App() {
 }
 
 export default App;
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 2c9c1c6 (Initialize project using Create React App)
