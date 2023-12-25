43fa7987e09962a9497c88a35e8ecfb27f392aec
Oleh Zinchenko <oleh.v.zinchenko@gmail.com>
2023-12-25 15:22:25 +0200

    Edit

--- src/App.js
+++ src/App.js
@@ -13,24 +13,24 @@ import {Scissors} from "./Components/Scissors"
 function App() {
   return (
       <div className="App">
-          {/*<h1>Number: odd</h1>*/}
-          {/*<Odd/>*/}
-          {/*<h1>String: lexics</h1>*/}
-          {/*<Lexics/>*/}
-          {/*<h1>Boolean</h1>*/}
-          {/*<Bool/>*/}
-          {/*<h1>Boolean: if</h1>*/}
-          {/*<BoolIf/>*/}
-          {/*<h1>Comparison: sizes</h1>*/}
-          {/*<Sizes/>*/}
-          {/*<h1>Ternary</h1>*/}
-          {/*<Gender/>*/}
-          {/*<h1>Prompt: or</h1>*/}
-          {/*<PromptOr/>*/}
-          {/*<h1>Confirm</h1>*/}
-          {/*<Confirm/>*/}
-          {/*<h1>Currency exchange</h1>*/}
-          {/*<CurrencyExchange/>*/}
+          <h1>Number: odd</h1>
+          <Odd/>
+          <h1>String: lexics</h1>
+          <Lexics/>
+          <h1>Boolean</h1>
+          <Bool/>
+          <h1>Boolean: if</h1>
+          <BoolIf/>
+          <h1>Comparison: sizes</h1>
+          <Sizes/>
+          <h1>Ternary</h1>
+          <Gender/>
+          <h1>Prompt: or</h1>
+          <PromptOr/>
+          <h1>Confirm</h1>
+          <Confirm/>
+          <h1>Currency exchange</h1>
+          <CurrencyExchange/>
           <h1>Scissors</h1>
           <Scissors/>
       </div>
