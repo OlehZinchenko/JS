806ddf761f55a78dfc0953f8ee023984a6643dc9
ZinchOleg <olegzzzinch@gmail.com>
2024-01-27 17:06:59 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+//---Literals---
+const cat = {
+    weight: 5,
+    color: 'black',
+    name: 'Kitty'
+}
+
+const car = {
+    model: 'BMW',
+    color: 'white',
+    year: 2010
+}
+
+const war = {
+    country: 'Ukraine',
+    year: 2014,
+    reason: 'Russia'
+}
+
+const pc = {
+    cpu: 'Intel',
+    ram: 8,
+    ssd: 256
+}
+
+//---Literals expand---
